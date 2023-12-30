f136014f7d4f340f1e1f789476ed2fca6e0e2044
matthieu debarge <debarge.matthieu@gmail.com>
2023-12-31 00:27:49 +0100

    formatage de la sidebar

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, HostListener } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RouterLink } from '@angular/router';
 import { AuthenticationService } from '../services/authentication.service';
@@ -12,8 +12,11 @@ import { AuthenticationService } from '../services/authentication.service';
 })
 export class NavbarComponent {
   score: number;
+  isMobile: boolean = false;
+  isExpanded: boolean = false;
 
-  constructor(public authService: AuthenticationService) { }
+
+  constructor(public authService: AuthenticationService, private eRef: ElementRef) { }
 
   ngOnInit() {
     this.authService.user$.subscribe(user => {
@@ -21,5 +24,48 @@ export class NavbarComponent {
         this.score = user.points;
       }
     });
+    this.checkWindowSize();
   }
+
+  @HostListener('window:resize', ['$event'])
+  onResize(event: any) {
+    this.checkWindowSize();
+  }
+
+  @HostListener('document:click', ['$event'])
+  clickout(event: any) {
+    if (!this.eRef.nativeElement.contains(event.target)) {
+      this.isExpanded = false;
+      let sidenav = document.querySelector('.sidenav');
+      if (sidenav) {
+        sidenav.classList.remove('isExpanded');
+      }
+    }
+  }
+
+  private checkWindowSize() {
+    this.isMobile = window.innerWidth < 768;
+  }
+
+  toggleExpand() {
+    this.isExpanded = !this.isExpanded;
+    let sidenav = document.querySelector('.sidenav');
+    if (sidenav === null) {
+      return;
+    }
+    if (this.isExpanded) {
+      sidenav.classList.add('isExpanded');
+    } else {
+      sidenav.classList.remove('isExpanded');
+    }
+  }
+
+  closeSidenav() {
+    this.isExpanded = false;
+    let sidenav = document.querySelector('.sidenav');
+    if (sidenav) {
+      sidenav.classList.remove('isExpanded');
+    }
+  }
+
 }
