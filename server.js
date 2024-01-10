21742058b85b31265be3badbea9ee3e27ddf88e2
matthieu debarge <debarge.matthieu@gmail.com>
2024-01-10 22:49:01 +0100

    port 80

--- server.js
+++ server.js
@@ -9,4 +9,4 @@ app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, '/dist/betpal/browser/index.html'));
 });
 
-app.listen(8080, () => console.log('HTTP Server running on port 8080'));
+app.listen(80, () => console.log('HTTP Server running on port 80'));
