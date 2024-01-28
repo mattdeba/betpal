3a122941c1d34b50cd417be97302beee61760d27
matthieu debarge <debarge.matthieu@gmail.com>
2024-01-28 14:35:02 +0100

    prod conf ok

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
+const port = process.env.PORT || 80;
+
+
 app.use('/', express.static(path.join(__dirname, 'dist', 'betpal')));
 
 app.get('*', (req, res) => {
   res.sendFile(path.join(__dirname, 'dist', 'betpal', 'index.html'));
 });
 
-app.listen(80, () => console.log('HTTP Server running on port 80'));
+app.listen(port, () => console.log(`HTTP Server running on port ${port}`));
