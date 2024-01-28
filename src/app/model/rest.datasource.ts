f1363c1d14638b0e31157c404fe6290879957813
matthieu debarge <debarge.matthieu@gmail.com>
2024-01-28 15:42:39 +0100

    set prod back url

--- src/app/model/rest.datasource.ts
+++ src/app/model/rest.datasource.ts
@@ -1,10 +1,11 @@
 import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { firstValueFrom } from 'rxjs';
+import { environment } from '../../environments/environment';
 
 @Injectable()
 export class RestDataSource {
-  baseUrl = 'http://localhost:3000';
+  baseUrl = environment.baseUrl;
   constructor(private http: HttpClient) { }
 
   async getUser(pseudo: string) {
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiUrl: 'http://localhost:3000'
+  apiUrl: 'http://localhost:3000',
+  baseUrl: 'https://betpalapp-09fe423923b7.herokuapp.com'
 };
