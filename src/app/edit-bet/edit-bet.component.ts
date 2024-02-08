1fc2e656fddeb05394ac15125508deeb74ba8d1b
Matthieu-D <matthieu.debarge@cuma.fr>
2024-02-08 21:12:31 +0100

    adaptation du mode d'édition aussi

--- src/app/edit-bet/edit-bet.component.ts
+++ src/app/edit-bet/edit-bet.component.ts
@@ -36,19 +36,30 @@ export class EditBetComponent {
     this.selectedGame = this.allGames.length > 0 ? this.allGames[0] : null;
   }
 
-  async onSubmit() {
-    const formInput = {
-      id: this.bet.id,
-      amount: this.betAmount,
-      target: this.betAmount + this.desiredAmount,
-      assertion: `Victoire de ${this.selectedGame.homeTeam} contre ${this.selectedGame.awayTeam}`,
-      gameId: this.selectedGame.id,
-      homeTeamWinner: this.selectedTeam === this.selectedGame.homeTeam,
-      creatorEmail: this.authService.getUser().email,
-    }
-    await this.dataSource.updateBet(formInput);
-    const updatedUser = await this.dataSource.getUser(this.authService.getUser().firstName);
-    this.authService.setUser(updatedUser);
-    await this.router.navigate(['/mybets']);
+async onSubmit() {
+  const otherTeam = this.selectedGame.homeTeam === this.selectedTeam ? this.selectedGame.awayTeam : this.selectedGame.homeTeam;
+  const formInput = {
+    id: this.bet.id,
+    amount: this.betAmount,
+    target: this.betAmount + this.desiredAmount,
+    assertion: `Victoire de ${this.selectedTeam} contre ${otherTeam} ${this.formatDate(this.selectedGame.dateTimeUTC)}`,
+    gameId: this.selectedGame.id,
+    homeTeamWinner: this.selectedTeam === this.selectedGame.homeTeam,
+    creatorEmail: this.authService.getUser().email,
+  }
+  await this.dataSource.updateBet(formInput);
+  const updatedUser = await this.dataSource.getUser(this.authService.getUser().firstName);
+  this.authService.setUser(updatedUser);
+  await this.router.navigate(['/mybets']);
+}
+
+  formatDate(dateUTC: string): string {
+    const date = new Date(dateUTC);
+    const day = date.getDate().toString().padStart(2, '0');
+    const month = (date.getMonth() + 1).toString().padStart(2, '0'); // Les mois sont indexés à partir de 0
+    const year = date.getFullYear();
+    const hours = date.getHours();
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    return `le ${day}/${month}/${year} à ${hours}:${minutes}`;
   }
 }
